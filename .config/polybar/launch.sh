bea0d3bb4a5c7a323c8fab0c672b522fe75e9af0
Martial Saunois <msaunois@deezer.com>
2019-11-08 10:19:01 +0100

    Fix several mistakes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,7 +5,7 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 X_BG=$(xrdb -query | grep color0 | tail -1 | cut -d"#" -f2)
 
-for m in $(xrandr --query | grep '\bconnected' | cut -d " " -f1); do
+for m in $(xrandr --listactivemonitors | tail -n2 | awk '{print $4}'); do
     MONITOR=$m BG="#aa${X_BG}" polybar -r main &
 done
 
