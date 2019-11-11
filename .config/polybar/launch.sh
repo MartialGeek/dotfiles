5676c9663b7ebacd40b854f046ea194895851a8e
Cyb3rD4d <cyb3rdad@protonmail.com>
2019-11-11 10:56:51 +0100

    Setup 3 polybars

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,9 +3,9 @@
 killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-X_BG=$(xrdb -query | grep color0 | tail -1 | cut -d"#" -f2)
-
-for m in $(xrandr --listactivemonitors | tail -n2 | awk '{print $4}'); do
-    MONITOR=$m BG="#aa${X_BG}" polybar -r main &
+for m in $(xrandr --query | grep '\bconnected' | cut -d " " -f1); do
+    MONITOR=$m polybar -r desktops &
+    MONITOR=$m polybar -r tray &
+    MONITOR=$m polybar -r mpd &
 done
 
